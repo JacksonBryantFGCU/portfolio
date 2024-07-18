ab86bf4f1f8c60d1b50166ce2e865075c718b9ca
Jackson Bryant <145219269+JacksonBryantFGCU@users.noreply.github.com>
2024-07-18 07:51:01 -0400

    Add files via upload

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,6 +5,8 @@ export const HERO_CONTENT = `I am a passionate front end developer with a knack 
 
 export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for user-friendly web applications. With 1 year of professional experience, I have worked with a variety of technologies, including React, Next.js, Tailwind, and SASS. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
+export const ABOUT_TEXT1 = `I am a dedicated and versatile front end developer with a passion for user-friendly web applications. With 1 year of professional experience, I have worked with a variety of technologies, including React, Next.js, Tailwind, and SASS. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
 export const EXPERIENCES = [
   {
     year: "2024 - Present",
@@ -18,14 +20,14 @@ export const EXPERIENCES = [
     role: "Student Math Tutor",
     company: " FGCU",
     description: `Helped students in math topics ranging from Algebra to Calculus II. Worked with students on a variety of topics by helping with Homework, Classwork, and anything else they needed help with.`,
-    skills: ["Problem Solving", "Comunication", "Math Skills"],
+    skills: ["Problem Solving", "Comunication", "Math"],
   },
   {
     year: "2023-2024",
     role: "Back of House Worker",
     company: " Chick-fil-A",
     description: `Worked in the Back of House for Chick-fil-A which included making the food, communicating with kitchen staff and front of house staff. `,
-    skills: ["Food Service", "Teamwork", "Communication"],
+    skills: ["Service", "Teamwork", "Communication"],
   },
 ];
 
@@ -35,18 +37,19 @@ export const PROJECTS = [
     image: project2,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information. Uses Framer Motion for all of the section animations. ",
-    skills: ["React", "Tailwind"],
-    demo: "https://github.com",
-    github: "https://microsoft.com",
+    skills: ["React", "Tailwind", "Framer Motion"],
+    demo: "https://jacksonbryantportfolio.netlify.app/",
+    github: "https://github.com",
   },
   {
     title: "Upgrades by Taylor Website",
     image: project1,
     description:
       "A fully functional web application that has an image gallery, Contact Form, as well as a testimonial carousel.",
-    skills: ["HTML", "CSS", "JS"],
+    skills: ["HTML", "CSS", "JS", "FormSpree"],
     demo: "https://linkedin.com",
-    github: "https://google.com",
+    github: "https://github.com",
+
   },
 ];
 
