2cb9f475dbe5120e7c35bd357419bac60a2a003e
Jackson L. Bryant <145219269+JacksonBryantFGCU@users.noreply.github.com>
2024-09-17 10:49:15 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,12 @@
 import project1 from "../assets/projects/project1.png";
 import project2 from "../assets/projects/project2.png";
+import SunstonePickleball from "../assets/projects/SunstonePickleball.png";
 
 export const HERO_CONTENT = `I am a passionate front end developer with a knack for crafting robust and scalable web applications. With 1 year of hands-on experience, I have started to develop my skills in front-end JavaScript technologies like React and Next.js, as well as CSS frameworks including Tailwind, and SASS. I strive to create websites that are not only functional but also have a great design to match. `;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for user-friendly web applications. With 1 year of professional experience, I have worked with a variety of technologies, including React, Next.js, Tailwind, and SASS. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for user-friendly web applications. With 1 year of professional experience, I have worked with a variety of technologies, including React, Next.js, Tailwind, React-Router-DOM, and Framer Motion. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy playing the piano, exploring new technologies, and contributing to open-source projects.`;
 
-export const ABOUT_TEXT1 = `I am a dedicated and versatile front end developer with a passion for user-friendly web applications. With 1 year of professional experience, I have worked with a variety of technologies, including React, Next.js, Tailwind, and SASS. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT1 = `I am currently a Sophomore at Florida Gulf Coast University where I am earning my B.S. in Software Engineering. My most relevant coursework is Calculus II, Statistical Methods, Programming I, and General Physics 1. I am currently taking Calculus III, Discrete Mathematics, Programming II, as well as Digital Systems and Architecture. I am currently expanding my skills by building projects, practicing LeetCode, and learning as much as I can on my own.`;
 
 export const EXPERIENCES = [
   {
@@ -33,13 +34,21 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
+    title: "Sunstone Pickleball Website",
+    image: SunstonePickleball,
+    description: "A website for the Sunstone Pickleball Website that includes a gallery carousel, player profile cards, events calendar, as well as a contact section.",
+    skills: ["React", "Tailwind", "React-Router", "Framer Motion"],
+    demo: "https://www.sunstonepickleball.com",
+    github: "https://github.com/JacksonBryantFGCU/SunstonePickleballClub",
+  },
+  {
     title: "Portfolio Website",
     image: project2,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information. Uses Framer Motion for all of the section animations. ",
     skills: ["React", "Tailwind", "Framer Motion"],
     demo: "https://jacksonbryantportfolio.netlify.app/",
-    github: "https://github.com",
+    github: "https://github.com/JacksonBryantFGCU/portfolio",
   },
   {
     title: "Upgrades by Taylor Website",
@@ -48,7 +57,7 @@ export const PROJECTS = [
       "A fully functional web application that has an image gallery, Contact Form, as well as a testimonial carousel.",
     skills: ["HTML", "CSS", "JS", "FormSpree"],
     demo: "https://linkedin.com",
-    github: "https://github.com",
+    github: "https://github.com/JacksonBryantFGCU/UpgradesbyTaylor",
 
   },
 ];
